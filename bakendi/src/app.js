eab354636fc84062c7b99d1d5e7163e28dd772a2
mikaelmanifreys <127792477+mikaelmanifreys@users.noreply.github.com>
2026-04-19 21:44:21 +0000

    Tengt við Render Web service

--- bakendi/src/app.js
+++ bakendi/src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 
@@ -9,7 +8,13 @@ const exercisesRouter = require('./routes/exercises');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'kraftur-vef2.netlify.app'
+  ],
+}));
+
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
